f3c391aa924a962da5549c201f53e55213915547
Mattan-Qwer <post@mnagl.eu>
2022-05-15 18:38:19 +0200

    add button and button function \n need refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,19 @@
 #define PIN A1
 #define PIN_ROTARY_A 5
 #define PIN_ROTARY_B 6
+#define BUTTON_PIN 9
 
 #define SECOND 100L // how many millis do a second have
 #define LEDUPDATETIME 100L
 
+#define LONG_PRESS_MILLIS 2000L
+
 #define DEBUG
 
 const int LED_COUNT = 35;
+
+// enums
+enum buttonPressed { NoPress, ShortPress, LongPress };
 
 // functions
 void set_timer(long time);
@@ -21,6 +27,9 @@ uint8_t UIntToColor(uint8_t rgbSelector, uint32_t color);
 void writeLEDs();
 void updateTimer();
 void restartTimer();
+enum buttonPressed buttonEvaluation();
+void buttonHandling();
+void checkPauseTime();
 
 // objects
 Adafruit_NeoPixel pixels(LED_COUNT, PIN, NEO_GRB + NEO_KHZ800);
@@ -31,12 +40,13 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Hello World!");
 #endif
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
   pixels.begin();
 }
 
+bool buttonPressed = false;
+
 unsigned long endTime = 20;
-unsigned long pauseTime = 20;
 const unsigned long TWENTY_MINUTES = SECOND * 60L * 20L;
 const unsigned long FIVE_MINUTES = SECOND * 60L * 5L;
 
@@ -48,14 +58,10 @@ bool pause = false;
 long oldRotaryPosition = -99;
 
 void loop() {
+  buttonHandling();
   restartTimer();
-
   updateTimer();
-#ifdef DEBUG
-  if (running) {
-    pause = true;
-  }
-#endif
+
   writeLEDs();
 #ifdef DEBUG
   long newRotaryPosition = myEnc.read();
@@ -70,7 +76,9 @@ void set_timer(long time) {
   uint8_t minutes = time / (SECOND * 60L);
   uint16_t overmillis = time % (SECOND * 60);
   uint8_t led_counter = 0;
-
+  for (int ii; ii < LED_COUNT; ii++) {
+    leds[ii] = 0;
+  }
   for (; led_counter < minutes and led_counter < LED_COUNT; led_counter++) {
     leds[led_counter] = colorToUInt(working ? 0 : 63, working ? 63 : 0, 0);
   }
@@ -113,6 +121,7 @@ uint8_t UIntToColor(uint8_t rgbSelector, uint32_t color) {
 unsigned long writeTime = 0;
 void writeLEDs() {
   if (writeTime <= millis()) {
+    pixels.clear();
     for (int ii = 0; ii < LED_COUNT; ii++) {
       pixels.setPixelColor(ii, pixels.Color(UIntToColor(2, leds[ii]),
                                             UIntToColor(1, leds[ii]),
@@ -128,13 +137,14 @@ void writeLEDs() {
 }
 
 void updateTimer() {
+  long remainingTime;
   if (!pause && running) {
-    long remainingTime = long(endTime - millis());
-    if (remainingTime > 0) {
-      set_timer(remainingTime);
-    } else {
-      running = false;
-    }
+    remainingTime = long(endTime - millis());
+  }
+  if (remainingTime > 0) {
+    set_timer(remainingTime);
+  } else {
+    running = false;
   }
 }
 
@@ -147,5 +157,76 @@ void restartTimer() {
       endTime = millis() + FIVE_MINUTES;
     }
     running = true;
+    pause = true;
   }
+}
+
+enum buttonPressed buttonEvaluation() {
+  const uint8_t retriggerSupressMillis = 2;
+  static bool button_pressure;
+  static unsigned long buttonTimer;
+  static unsigned long retriggerSupress = 0;
+  enum buttonPressed returnVal = NoPress;
+
+  if (retriggerSupress < millis()) {
+    if (!digitalRead(BUTTON_PIN) && !button_pressure) {
+      buttonTimer = millis();
+      button_pressure = true;
+    }
+
+    if (button_pressure && digitalRead(BUTTON_PIN)) {
+      if ((buttonTimer + LONG_PRESS_MILLIS) < millis()) {
+        returnVal = LongPress;
+      } else {
+        returnVal = ShortPress;
+      }
+#ifdef DEBUG
+      Serial.println(retriggerSupress);
+
+      Serial.println(millis());
+      Serial.println((returnVal == LongPress) ? "long press released"
+                                              : "short press released");
+#endif
+      button_pressure = false;
+      retriggerSupress = millis() + retriggerSupressMillis;
+    }
+  }
+  return returnVal;
+}
+
+void buttonHandling() {
+  enum buttonPressed event = buttonEvaluation();
+  if (running) {
+    switch (event) {
+    case ShortPress:
+      pause = !pause;
+      break;
+
+    case LongPress:
+      pause = true;
+      running = false;
+      break;
+
+    default:
+      break;
+    }
+  } else {
+    if (event == ShortPress) {
+      pause = false;
+      running = true;
+    }
+  }
+}
+
+bool wasPaused = false;
+unsigned long pauseTime = 0L;
+
+void checkPauseTime() {
+
+  if (wasPaused && !pause) {
+    endTime += millis() - pauseTime;
+  } else if (!wasPaused && pause) {
+    pauseTime = millis();
+  }
+  wasPaused = pause;
 }
